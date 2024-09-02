277c3a38f677bd1a89668093233443479f1bdf81
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-02 19:31:43 +0200

    tested Student class

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-public class Student {
+class Student {
     private String rollNo = new String();
     private String name = new String();
     public Student(String r, String n){
@@ -9,13 +9,21 @@ public class Student {
         this.rollNo=r;
         this.name=n;
     }
+    public String getData(){
+        return "The person's roll number is "+ this.rollNo + " and name is "+this.name;
+    }
     public void displayStudent(){
         System.out.println(this.rollNo);
         System.out.println(this.name);
-    }  
+    }                  
+}
+public class Test {
     public static void main(String args[]){
         Student s1 = new Student("abc08D","shoaib");
         s1.setData("updated-rollno-fghr354", "updated-name-Ali");
+        String receivedData = new String();
+        receivedData = s1.getData();
         s1.displayStudent();
-    }            
+        System.out.println(receivedData);
+    }
 }
