e3e4d10c07d4978b0e8eb875b9637cdcfa373eac
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-03 11:56:21 +0200

    polymorphism

--- EmpTeachTest.java
+++ EmpTeachTest.java
@@ -1,19 +1,22 @@
 
 class Employee {
 
-    int id;
+    String id;
     String name;
 
-    // Employee() {
-    //     Employee(10, "shoaib");
-    // }
-    Employee(int id, String name) {
+    Employee(String id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    Employee() {
+        this("empId-124", "Bjoern");
+    }
+
     void display() {
+        System.out.println("Employee class");
         System.out.println(this.id + " has " + this.name);
+        System.out.println("----------------------");
     }
 
 }
@@ -23,19 +26,24 @@ class Teacher extends Employee {
     String qual = "Web Developer";
 
     Teacher() {
-        super(10, "shoaib");
+        super("bjoern-w11", "Bjoernn");
     }
 
     void display() {
+        System.out.println("Teacher class");
         System.out.println(this.name + " has " + this.id + ". He is a " + this.qual);
+        System.out.println("----------------------");
     }
-
 }
 
 public class EmpTeachTest {
 
     public static void main(String args[]) {
-        Teacher t = new Teacher();
-        t.display();
+        Employee e1 = new Employee();
+        e1.display(); // empId-124, Bjoern
+        Teacher t1 = new Teacher();
+        t1.display(); // bjoern-w11, Bjoernn
+        Employee e2 = new Teacher();
+        e2.display();// bjoern-w11, Bjoernn
     }
 }
